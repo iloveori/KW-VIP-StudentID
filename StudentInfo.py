d360523475d2b3b484c6e9c99e7505aa80641827
DESKTOP-JVI18I3\동글동글오리 <whiteblack4@naver.com>
2020-04-21 09:57:37 +0900

    StudentInfo

---
+++ StudentInfo.py
@@ -0,0 +1,80 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtGui import *
+import qimage2ndarray
+import numpy as np
+import cv2
+
+basic_ui = uic.loadUiType("StudentInfo.ui")[0]
+
+class WindowClass(QMainWindow, basic_ui) :
+    image = QImage()
+
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+        
+        self.pushButton.clicked.connect(lambda : self.ShowInfo(1))
+        self.pushButton_2.clicked.connect(lambda : self.ShowInfo(2))
+        self.pushButton_3.clicked.connect(lambda : self.ShowInfo(3))
+        self.pushButton_4.clicked.connect(lambda : self.ShowInfo(4))
+        self.pushButton_5.clicked.connect(lambda : self.ShowInfo(5))
+        self.pushButton_6.clicked.connect(lambda : self.ShowInfo(6))
+        self.pushButton_7.clicked.connect(lambda : self.ShowInfo(7))
+        self.pushButton_8.clicked.connect(lambda : self.ShowInfo(8))
+        self.pushButton_9.clicked.connect(lambda : self.ShowInfo(9))
+        self.pushButton_10.clicked.connect(lambda : self.ShowInfo(10))
+
+    def ShowInfo(self, num):
+        studentid = ''
+
+        if num == 1: #이로사
+            studentid = ""
+
+        elif num == 2: #권나성
+            studentid = ""
+
+        elif num == 3: #김나현
+            studentid = ""
+
+        elif num == 4: #서성원
+            studentid = ""
+
+        elif num == 5: #황승언
+            studentid = ""
+
+        elif num == 6: #이성호
+            studentid = ""
+
+        elif num == 7: #권동욱
+            studentid = ""
+
+        elif num == 8: #한정수
+            studentid = ""
+
+        elif num == 9: #손정현
+            studentid = ""
+
+        else :         #금빛나
+            studentid = ""
+            
+
+        buttonReply = QMessageBox.information(
+            self, "StudentInfo", studentid, 
+            QMessageBox.Yes)
+ 
+    
+        if buttonReply == QMessageBox.Yes:
+            print('Yes clicked.')
+        else:
+            print('No clicked.')
+
+        self.show()
+        
+if __name__ == "__main__" :
+    app = QApplication(sys.argv) 
+    myWindow = WindowClass() 
+    myWindow.show()
+    app.exec_()
+
